1fe0ae0ac83171097acce3415035a89b6cb6549e
effisso <95202838+effisso@users.noreply.github.com>
2022-01-09 08:16:57 -0500

    add http files

---
+++ mc/http_test.go
@@ -0,0 +1,32 @@
+package mc_test
+
+import (
+	"mcmods/mc"
+	"net/http"
+	"net/url"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("HTTP", func() {
+	Context("http client", func() {
+		It("follows redirects", func() {
+			testPath := "http://www.google.com/"
+			req := &http.Request{
+				URL: &url.URL{
+					Path: testPath,
+				},
+			}
+
+			client := mc.NewHTTPClient()
+			clientGetter, ok := client.Getter.(*http.Client)
+			Expect(ok).To(BeTrue())
+
+			err := clientGetter.CheckRedirect(req, []*http.Request{})
+
+			Expect(err).To(BeNil())
+			Expect(req.URL.Opaque).To(Equal(testPath))
+		})
+	})
+})
